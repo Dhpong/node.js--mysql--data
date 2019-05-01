1354413f9d2f992f552a8bdb6e9f7b8e64b5e26b
Dhpong <34147965+Dhpong@users.noreply.github.com>
2019-05-01 15:58:09 +0800

    项目构建
    初步HTML页面

---
+++ node-web/src/components/HelloWorld.vue
@@ -0,0 +1,341 @@
+<template>
+  <div class="hello">
+
+    <div>
+      <!-- 分类 -->
+      <ul class="list">
+        <li class="SG_dot">
+          <a href="https://vuejs.org" target="_blank">全部博文</a>
+          <em>( 301) </em>
+        </li>
+
+        <li class="SG_dot">
+          <a href="https://forum.vuejs.org" target="_blank">祖国大陆</a>~
+          <em>( 301)</em>
+        </li>
+
+        <li class="SG_dot">
+          <a href="http://....1.html" target="_blank">台湾</a>~
+          <em>( 301)</em>
+        </li>
+         <li class="SG_dot">
+          <a href="https://github.com/vuejs/awesome-vue" target="_blank">广澳</a>~
+          <em>( 301)</em>
+        </li>
+         <li class="SG_dot">
+          <a href="http://vue-loader.vuejs.org/" target="_blank">东南亚</a>~
+          <em>( 301)</em>
+        </li>
+         <li class="SG_dot">
+          <a href="http://vuex.vuejs.org/" target="_blank">澳大利亚</a>~
+          <em>( 301)</em>
+        </li>
+         <li class="SG_dot">
+          <a href="http://router.vuejs.org/" target="_blank">日本</a>~
+          <em>303</em>
+        </li>
+         <li class="SG_dot">
+          <a href="http://vuejs-templates.github.io/webpack/" target="_blank">美国</a>~
+          <em>( 301)</em>
+        </li>
+        
+        <li class="SG_dot">
+          <a href="https://twitter.com/vuejs" target="_blank">MY生活</a>~
+          <em>( 301)</em>
+        </li>
+
+        <li class="SG_dot">
+          <a href="https://chat.vuejs.org" target="_blank">小生活</a>~
+          <em>( 301)</em>
+        </li>
+         
+      </ul>
+    </div>
+    
+
+    
+    <h2>Essential Links</h2>
+
+    <!-- 博文 -->
+    <div class="article">
+      <ul>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt">
+              <strong>荐</strong> &nbsp;
+             同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085)  &nbsp;&nbsp;2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp;  2019.9.5 08:12
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp; 2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085)&nbsp;&nbsp;  2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085)&nbsp;&nbsp;  2019.9.5 08:12 
+          </span>
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085)&nbsp;&nbsp;  2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp;  2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp;  2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp;2019.9.5 08:12 
+          </span>
+
+        </li>
+        <li>
+          
+          <a href="" target="_blank">
+            <span class="txt"><strong>荐</strong>&nbsp;
+            同里古镇:在编写超文本标记语言（标准通用标记语言下的一个应用）页面时，直接使用spac空格将会被忽略，这时候应该使用不换行空格 &nbsp; 用来占位。超文本标记语言，标准通用标记语言下的一个应用。“超文本”就是指页面内可以包含图片、链接，甚至音乐、程序等非文字元素。</span>
+            <img src="../assets/logo.png" width="20" height="20" title="">
+          </a>
+
+          <span class="fr">
+            (68/2085) &nbsp;&nbsp; 2019.9.5 08:12
+          </span>
+
+        </li>
+      </ul>
+      
+    </div>
+
+    <!-- 首页 -->
+    <div class="blog">
+      <ul>
+        <h1>博客论文</h1>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+        <li>
+          <a href="">403是因为服务器拒绝了你的地址请求，或者你根本没权限访问网站，提供身份验证也没用，也就是说，用户被禁止访问了。然而除非与Web服务器管理员联系，否则一旦遇到403状态码都无法自行解决。</a>
+        </li>
+      </ul>
+      
+
+    </div>
+
+    <!-- 文章 -->
+    <div class="article_txt">
+      <h2>CSS行高——line-height - 谦行 - 博客园</h2>
+      <p>excel是我们平时办公操作中最常用的办公软件，用来制作表格是很方便的，提到表格就会涉及到表格的行高，那么excel中行高怎么设置呢？下面介绍两种excel中设置行高的方法，希望对你有所帮助。</p>
+      <p>
+        excel是我们平时办公操作中最常用的办公软件，用来制作表格是很方便的，提到表格就会涉及到表格的行高，那么excel中行高怎么设置呢？下面介绍两种excel中设置行高的方法，希望对你有所帮助。
+      </p>
+      <p>
+        excel是我们平时办公操作中最常用的办公软件，用来制作表格是很方便的，提到表格就会涉及到表格的行高，那么excel中行高怎么设置呢？下面介绍两种excel中设置行高的方法，希望对你有所帮助。
+      </p>
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list{
+  width:200px;
+  overflow:hidden;
+}
+.list li{
+  float:left;
+  width:100%;
+}
+
+.article{
+  width:600px;
+}
+.article ul{
+  width:100%;
+
+}
+.article ul li{
+  width:100%;  
+
+}
+
+.fr{
+  float:right;
+}
+
+.txt{
+  width: 250px;/*需要配合宽度来使用*/
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  display:inline-block;/*如果是一个inline标签，还需要加入这一行代码，因为对于inline标签设置width没有用。这里是li标签，本来就是block，因此不需要。*/
+  float:left;
+  line-height:20px;
+}
+
+.article a{
+  width:300px;
+}
+.article img{
+  float:left;
+
+}
+
+
+.blog{
+  width:500px;
+}
+
+.blog ul{
+  width:100%;
+}
+.blog ul li{
+  width:100%;
+}
+.blog a{
+  width:50%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  display:inline-block;/*如果是一个inline标签，还需要加入这一行代码，因为对于inline标签设置width没有用。这里是li标签，本来就是block，因此不需要。*/
+}
+
+.article_txt p{
+  text-indent:24px;
+}
+
+
+
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
